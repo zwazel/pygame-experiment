cbca2a41477e24aa339a4c5e7d9ff7a79f1aceff
Tim Irmler - Laptop <zwazel@hotmail.com>
2023-12-03 22:48:47 +0100

    squares and collisions

--- main.py
+++ main.py
@@ -7,7 +7,24 @@ pygame.init()
 # Set up the display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Hello, Pygame World!")
+pygame.display.set_caption("Moving Squares")
+
+# Set up the red square
+red_square_size = 50
+red_square_color = (255, 0, 0)  # Red
+red_square_x = width // 2 - red_square_size // 2
+red_square_y = 10  # Start position near the top
+red_square_speed = 5
+
+# Set up the black square
+black_square_size = 50
+black_square_color = (0, 0, 0)  # Black
+black_square_x = width // 2 - black_square_size // 2
+black_square_y = height - black_square_size - 10  # Start position near the bottom
+black_square_speed = 5
+
+# Set up the clock for controlling the frame rate
+clock = pygame.time.Clock()
 
 # Run the game loop
 while True:
@@ -16,16 +33,41 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # Move the red square down
+    red_square_y += red_square_speed
+
+    # Wrap around to the top when the red square goes off the bottom of the screen
+    if red_square_y > height:
+        red_square_y = -red_square_size
+
+    # Move the black square based on keyboard input
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a] and black_square_x > 0:
+        black_square_x -= black_square_speed
+    if keys[pygame.K_d] and black_square_x < width - black_square_size:
+        black_square_x += black_square_speed
+
+    # Collision detection
+    red_rect = pygame.Rect(red_square_x, red_square_y, red_square_size, red_square_size)
+    black_rect = pygame.Rect(black_square_x, black_square_y, black_square_size, black_square_size)
+
+    if red_rect.colliderect(black_rect):
+        # Reset the positions of both squares
+        red_square_y = 10
+        black_square_x = width // 2 - black_square_size // 2
+        black_square_y = height - black_square_size - 10
+
     # Clear the screen
     screen.fill((255, 255, 255))  # Fill with white color
 
-    # Draw "Hello, World!" on the screen
-    font = pygame.font.Font(None, 36)
-    text = font.render("Hello, Pygame World!", True, (0, 0, 0))  # Render text in black color
-    screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
+    # Draw the red square
+    pygame.draw.rect(screen, red_square_color, (red_square_x, red_square_y, red_square_size, red_square_size))
+
+    # Draw the black square
+    pygame.draw.rect(screen, black_square_color, (black_square_x, black_square_y, black_square_size, black_square_size))
 
     # Update the display
     pygame.display.flip()
 
     # Control the frame rate
-    pygame.time.Clock().tick(60)
+    clock.tick(60)
