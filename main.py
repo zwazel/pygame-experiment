1a4f8372b5c782959d2a55ca51276d5cf16f0743
Tim Irmler - Laptop <zwazel@hotmail.com>
2023-12-03 23:54:59 +0100

    def main

--- main.py
+++ main.py
@@ -2,124 +2,130 @@ import sys
 
 import pygame
 
-# Initialize Pygame
-pygame.init()
 
-# Set up the display
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pygame Experiment")
+def main():
+    # Initialize Pygame
+    pygame.init()
 
-# Set up fonts
-font = pygame.font.Font(None, 36)
+    # Set up the display
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pygame Experiment")
 
-# Define colors
-white = (255, 255, 255)
-red = (255, 0, 0)
-black = (0, 0, 0)
+    # Set up fonts
+    font = pygame.font.Font(None, 36)
 
-# Set up the clock for controlling the frame rate
-clock = pygame.time.Clock()
+    # Define colors
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    black = (0, 0, 0)
 
-# Define game states
-MENU = 0
-GAME = 1
-current_state = MENU
+    # Set up the clock for controlling the frame rate
+    clock = pygame.time.Clock()
 
-# Set up the red square
-red_square_size = 50
-red_square_color = red
-red_square_x = width // 2 - red_square_size // 2
-red_square_y = 10  # Start position near the top
-red_square_speed = 5
+    # Define game states
+    MENU = 0
+    GAME = 1
+    current_state = MENU
 
-# Set up the black square
-black_square_size = 50
-black_square_color = black
-black_square_x = width // 2 - black_square_size // 2
-black_square_y = height - black_square_size - 10  # Start position near the bottom
-black_square_speed = 5
+    # Set up the red square
+    red_square_size = 50
+    red_square_color = red
+    red_square_x = width // 2 - red_square_size // 2
+    red_square_y = 10  # Start position near the top
+    red_square_speed = 5
 
-# Set up the vertical lines
-line1_x = width // 4
-line2_x = 3 * width // 4
-line_width = 10
+    # Set up the black square
+    black_square_size = 50
+    black_square_color = black
+    black_square_x = width // 2 - black_square_size // 2
+    black_square_y = height - black_square_size - 10  # Start position near the bottom
+    black_square_speed = 5
 
-# Define the start button
-button_rect = pygame.Rect(width // 2 - 100, height // 2 - 30, 200, 60)
+    # Set up the vertical lines
+    line1_x = width // 4
+    line2_x = 3 * width // 4
+    line_width = 10
 
-# Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    # Define the start button
+    button_rect = pygame.Rect(width // 2 - 100, height // 2 - 30, 200, 60)
 
-        # Check for mouse button click
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x, y = event.pos
-            if button_rect.collidepoint(x, y) and current_state == MENU:
-                # Switch to the game state when the user clicks the button in the main menu
-                current_state = GAME
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    # Main menu
-    if current_state == MENU:
-        screen.fill(white)
-        pygame.draw.rect(screen, black, button_rect)
-        text = font.render("Start", True, white)
-        screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
+            # Check for mouse button click
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
+                if button_rect.collidepoint(x, y) and current_state == MENU:
+                    # Switch to the game state when the user clicks the button in the main menu
+                    current_state = GAME
 
-    # Game state
-    elif current_state == GAME:
-        # Move the red square down
-        red_square_y += red_square_speed
+        # Main menu
+        if current_state == MENU:
+            screen.fill(white)
+            pygame.draw.rect(screen, black, button_rect)
+            text = font.render("Start", True, white)
+            screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
 
-        # Wrap around to the top when the red square goes off the bottom of the screen
-        if red_square_y > height:
-            red_square_y = -red_square_size
+        # Game state
+        elif current_state == GAME:
+            # Move the red square down
+            red_square_y += red_square_speed
 
-        # Move the black square based on keyboard input
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] or keys[pygame.K_LEFT] and black_square_x > 0:
-            black_square_x -= black_square_speed
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT] and black_square_x < width - black_square_size:
-            black_square_x += black_square_speed
+            # Wrap around to the top when the red square goes off the bottom of the screen
+            if red_square_y > height:
+                red_square_y = -red_square_size
 
-        # Ensure the player stays centered between the two lines
-        if black_square_x < line1_x:
-            black_square_x = line1_x
-        elif black_square_x + black_square_size > line2_x:
-            black_square_x = line2_x - black_square_size
+            # Move the black square based on keyboard input
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_a] or keys[pygame.K_LEFT] and black_square_x > 0:
+                black_square_x -= black_square_speed
+            if keys[pygame.K_d] or keys[pygame.K_RIGHT] and black_square_x < width - black_square_size:
+                black_square_x += black_square_speed
 
-        # Collision detection
-        red_rect = pygame.Rect(red_square_x, red_square_y, red_square_size, red_square_size)
-        black_rect = pygame.Rect(black_square_x, black_square_y, black_square_size, black_square_size)
+            # Ensure the player stays centered between the two lines
+            if black_square_x < line1_x:
+                black_square_x = line1_x
+            elif black_square_x + black_square_size > line2_x:
+                black_square_x = line2_x - black_square_size
 
-        if red_rect.colliderect(black_rect):
-            # Reset the positions of both squares
-            red_square_y = 10
-            black_square_x = width // 2 - black_square_size // 2
-            black_square_y = height - black_square_size - 10
+            # Collision detection
+            red_rect = pygame.Rect(red_square_x, red_square_y, red_square_size, red_square_size)
+            black_rect = pygame.Rect(black_square_x, black_square_y, black_square_size, black_square_size)
 
-            # Switch back to the menu state
-            current_state = MENU
+            if red_rect.colliderect(black_rect):
+                # Reset the positions of both squares
+                red_square_y = 10
+                black_square_x = width // 2 - black_square_size // 2
+                black_square_y = height - black_square_size - 10
 
-        # Clear the screen
-        screen.fill(white)
+                # Switch back to the menu state
+                current_state = MENU
 
-        # Draw the vertical lines
-        pygame.draw.rect(screen, black, (line1_x - line_width // 2, 0, line_width, height))
-        pygame.draw.rect(screen, black, (line2_x - line_width // 2, 0, line_width, height))
+            # Clear the screen
+            screen.fill(white)
 
-        # Draw the red square
-        pygame.draw.rect(screen, red_square_color, (red_square_x, red_square_y, red_square_size, red_square_size))
+            # Draw the vertical lines
+            pygame.draw.rect(screen, black, (line1_x - line_width // 2, 0, line_width, height))
+            pygame.draw.rect(screen, black, (line2_x - line_width // 2, 0, line_width, height))
 
-        # Draw the black square
-        pygame.draw.rect(screen, black_square_color, (black_square_x, black_square_y,
-                                                      black_square_size, black_square_size))
+            # Draw the red square
+            pygame.draw.rect(screen, red_square_color, (red_square_x, red_square_y, red_square_size, red_square_size))
 
-    # Update the display
-    pygame.display.flip()
+            # Draw the black square
+            pygame.draw.rect(screen, black_square_color, (black_square_x, black_square_y,
+                                                          black_square_size, black_square_size))
 
-    # Control the frame rate
-    clock.tick(60)
+        # Update the display
+        pygame.display.flip()
+
+        # Control the frame rate
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    main()
